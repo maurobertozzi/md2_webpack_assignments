2ec1d505de61ed94c31ae8e7117186446e4f3cde
maurobertozzi <maurobertozzi11@gmail.com>
2017-03-13 14:57:57 +0100

    opdrachten

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
+
 /**
  * Uitleg van deze opdrachten:
  * Kijk hoever je komt met deze opdrachten. Ik zal deze lijst in de toekomst nog wat uitbreiden met nieuwere opdrachten
  * Je mag me bij elke opdracht om hulp vragen
  * Sowieso wil ik je werk zien. Dus loop even bij me langs als je 1 of meerdere opdrachten af hebt
  */
-
+console.log(555);
 /**
  * Opdracht 1: primitives
  * Maak voor de volgende primitives 5 goed bedachte variabelen per primitive
@@ -12,11 +13,28 @@
  * 5 variabelen met daarin een string
  * 5 variabelen met daarin een number
  * 5 variabelen met daarin een boolean
- * 
+ 
  * Geef ze een goede engelse naam en sla er iets in op wat een goed voorbeeld geeft
  * van Media Development producten (apps, sites, etc.)
  * Bijvoorbeeld een string variabele met de naam 'teacherName' en als waarde 'Berend'
  */
+ 	var teacherName = "Berend";
+ 	var mentor = "Rosmerta";
+ 	var rdteacher = "ed";
+ 	var bacteacher = "hugo";
+ 	var javateacher = "joey";
+
+ 	var housenumber = 43;
+ 	var mobile = +3164413658;
+ 	var age = 17;
+ 	var dogsageMonths = "4";
+ 	var sisterage = "19";
+
+ 	var hungry = true;
+ 	var tired = true;
+ 	var wantToGoHome = true;
+ 	var wantFood = true;
+ 	var wantPizza = true;
 
 /**
  * Opdracht 2: Arrays
@@ -26,11 +44,24 @@
  * Bijvoorbeeld een Array met de naam 'students' en als waarde ['Berend', "Erwin']
  */
 
+  var cars = ['Volvo', 'Renault', 'Ferrari'];
+console.log(cars.length);
+
+	var animal = ['Cat', 'Dog', 'Elephant', 'Tiger', 'Mouse', 'Bird'];
+	console.log(animal.length);
+
+	var ducks  = ['kwik', 'kwek', 'kwak'];
+	console.log(ducks.length);
+
 /**
  * Opdracht 3: Arrays push
  * Maak een lege array aan
  * push er een string in
  */
+
+ let A = [];
+ A.push("kaas");
+ console.log(A);
 
 /**
  * Opdracht 4: Array splice
@@ -38,13 +69,27 @@
  * Gooi de naam op de 1e plek weg
  * Gooi de naam op de 3e plek weg
  */
-
+ let B = ["harry","henk","Herman"];
+ B.splice(0,1);
+ B.splice(1,1);
+ console.log(B);
 /**
  * Opdracht 5: Objects
  * Maak van 10 objecten uit je dagelijkse leven een object
  *
  * Bijvoorbeeld een object met de gegevens van je mobiel zoals welk merk, type en hoe oud hij is
  */
+ let mobilephone = {type:"OnePlus", model:"3T", color:"Black"};
+ let laptop = {type:"MSI", model:"GT722QE", color:"Black"};
+ let mouse = {type:"Razer", model:"NageHexV2", color:"Black"};
+ let consoles = {type:"Xbox", model:"One", color:"Black"};
+ let Headset = {type:"Astro", model:"A40tr", color:"White"};
+ let carFather = {type:"Kia", model:"ceed", color:"brown"};
+ let carMother = {type:"Renault", model:"megane", color:"gray"};
+ let Father = {FirstName:"Sergio", LastName:"Bertozzi", age:"52"};
+ let Mother = {name:"Irene", LastName:"Bertozzi", age:"50"};
+ let sister = {name:"Denise", LastName:"Bertozzi", age:"19"};
+
 
 /**
  * Opdracht 6: Functions
